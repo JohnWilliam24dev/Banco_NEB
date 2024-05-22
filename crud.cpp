be208354b5372ac9da1139d64fdcf1013507d671
JohnWilliam24dev <163377183+JohnWilliam24dev@users.noreply.github.com>
2024-05-22 16:17:23 -0300

    update System DB(0.0.3)

--- crud.cpp
+++ crud.cpp
@@ -3,7 +3,7 @@
 #include <string.h>
 #include <sys/stat.h>
 
-void insert_client_legal_entire(char nome[],char data_de_nascimento[],char CNPJ[],float saldo,float credito){
+void insert_client_legal_entire(char nome[],char senha[],int pin,char data_de_nascimento[],char CNPJ[],char telefone[],char endereco[],float saldo,float credito){
 
 
     
@@ -21,18 +21,45 @@ void insert_client_legal_entire(char nome[],char data_de_nascimento[],char CNPJ[
     file=fopen(path,"w");
     
     fprintf(file,"nome=%s\n",nome);
+    fprintf(file,"senha=%s\n",senha);
+    fprintf(file,"pin=%d\n",pin);
     fprintf(file,"CNPJ=%s\n",CNPJ);
     fprintf(file,"credito=%f\n",credito);
+    fprintf(file,"Telefone=%s\n",telefone);
+    fprintf(file,"endereco=%s\n",endereco);
     fprintf(file,"saldo=%f\n",saldo);
     fprintf(file,"data=%s\n",data_de_nascimento);
 
 }
 
+void insert_client_natural_person(char nome[],char senha[],int pin,char data_de_nascimento[],char CPF[],char telefone[],char endereco[],float saldo,float credito){
+
+int f=mkdir("./PF",S_IRWXU);
+    if (f==0){
+        system("mkdir PF");
+        system("clear");
+    }
+    char path[100]="./PF/userPF";
+    strcat(path,CPF);
+    char ext[15]={".txt"};
+    strcat(path,ext);
+    FILE *file;
+    file=fopen(path,"w");
+    
+    fprintf(file,"nome=%s\n",nome);
+    fprintf(file,"senha=%s\n",senha);
+    fprintf(file,"pin=%d\n",pin);
+    fprintf(file,"CNPJ=%s\n",CPF);
+    fprintf(file,"credito=%f\n",credito);
+    fprintf(file,"Telefone=%s\n",telefone);
+    fprintf(file,"endereco=%s\n",endereco);
+    fprintf(file,"saldo=%f\n",saldo);
+    fprintf(file,"data=%s\n",data_de_nascimento);
+}
+
 int main(){
-    insert_client_legal_entire("João","01/02/1990","39278382000155",11234,30000);
-    insert_client_legal_entire("Pedro","30/03/2004","46792439000103",11234,30000);
-    insert_client_legal_entire("Daniel","11/11/2001","38646218000190",11234,30000);
-    insert_client_legal_entire("Sayd","01/02/1990","20373525000180",11234,30000);
-    insert_client_legal_entire("Matheus","01/02/1990","63167366000170",11234,30000);
-    insert_client_legal_entire("Matheus","01/02/1990","23186418000130",11234,30000);
+    insert_client_legal_entire("João","herobrine45",1234,"01/02/1990","39278382000155","75 99882-4924","rua skywalker",11234,30000);
+    insert_client_natural_person("Pedro","herobrine45",1234,"30/03/2004","46792439000103","75 99882-4924","rua skywalker",11234,30000);
+    insert_client_natural_person("Daniel","herobrine45",1234,"11/11/2001","38646218000190","75 99882-4924","rua skywalker",11234,30000);
+    
 }
