ecce936c89515cb382fb09e4451bc1394fc40777
JohnWilliam24dev <163377183+JohnWilliam24dev@users.noreply.github.com>
2024-05-22 12:03:58 -0300

    System DB(0.0.2)
    Aprimoramento no banco de dados, função Insert já preparada para uso em outras areas do codigo sujeito a mudanças

--- crud.cpp
+++ crud.cpp
@@ -1,18 +1,33 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
+void insert_client_legal_entire(char nome[],char data_de_nascimento[],char CNPJ[],float saldo,float credito){
+
+
+    
+    
+    
+    char path[100]="./PJ/userPJ";
+    strcat(path,CNPJ);
+    char ext[15]={".txt"};
+    strcat(path,ext);
+    FILE *file;
+    file=fopen(path,"a");
+    
+    fprintf(file,"nome=%s\n",nome);
+    fprintf(file,"CNPJ=%s\n",CNPJ);
+    fprintf(file,"credito=%f\n",credito);
+    fprintf(file,"saldo=%f\n",saldo);
+    fprintf(file,"data=%s\n",data_de_nascimento);
+
+}
 
 int main(){
-    struct userPJ
-    {
-        char nome;
-        char data_de_nascimento;
-        char CNPJ;
-        float saldo;
-        float credito;
-    };
-    system("mkdir PF");
-    FILE *file;
-    file=fopen(("./PF/userPF.txt"),"a");
-    fprintf(file,"nome=%s","João");
-
+    insert_client_legal_entire("João","01/02/1990","39278382000155",11234,30000);
+    insert_client_legal_entire("Pedro","30/03/2004","46792439000103",11234,30000);
+    insert_client_legal_entire("Daniel","11/11/2001","38646218000190",11234,30000);
+    insert_client_legal_entire("Sayd","01/02/1990","20373525000180",11234,30000);
+    insert_client_legal_entire("Matheus","01/02/1990","63167366000170",11234,30000);
+    insert_client_legal_entire("Matheus","01/02/1990","23186418000130",11234,30000);
 }
