69de6c713137e665c460adfbda6a97807dcd48eb
MATHEUS ALCANTARA SILVA <tetheu.10.alcantarasilva@gmail.com>
2024-05-28 12:01:46 -0300

    Atualizar o CADASTROPJ.cpp

--- CADASTROPJ.cpp
+++ CADASTROPJ.cpp
@@ -1,5 +1,6 @@
-#include<stdio.h>
-#include<stdlib.h>
+
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
 void cadastro1() {
@@ -8,37 +9,72 @@ void cadastro1() {
     char nomepropietario[35];
     float idadeempresa;
     float idadepropietario;
-    int telefone;
-    char senha;
+    char senha[20];
+    char CPF[12];  
+    char telefone[10]; 
 
-    printf("Por favor, digite seu nome(propietario): ");
+    printf("Por favor, digite seu nome (proprietario): ");
     scanf("%s", nomepropietario);
-    
+
     printf("\nPor favor, digite o nome da empresa: ");
     scanf("%s", nomeempresa);
+
     
-    printf("\nPor favor, digite o cnpj: ");
-    scanf("%s", cnpj);
-    
- do {
-        printf("\nPor favor, digite o CNPJ: ");
+    do {
+        printf("\nPor favor, digite o CNPJ (14 dígitos): ");
         scanf("%s", cnpj);
 
-        if(strlen(cnpj) == 14) {
+        if (strlen(cnpj) == 14) {
             printf("CNPJ válido.\n");
             break;
         } else {
-            printf("CNPJ inválido. Por favor digite novamente.\n");
+            printf("CNPJ inválido. Por favor, digite novamente.\n");
         }
     } while (1);
-    
+
     printf("\nPor favor, digite a idade da empresa: ");
     scanf("%f", &idadeempresa);
-    
-    printf("\nPor favor, digite a idade do propietrio: ");
+
+    printf("\nPor favor, digite a idade do proprietário: ");
     scanf("%f", &idadepropietario);
-  printf("\n Por favor, crie uma senha:");
-   
+
+    
+    do {
+        printf("\nPor favor, digite o CPF (11 dígitos): ");
+        scanf("%s", CPF);
+
+        if (strlen(CPF) == 11) {
+            printf("CPF válido.\n");
+            break;
+        } else {
+            printf("CPF inválido. Por favor, digite novamente.\n");
+        }
+    } while (1);
+
+    
+    do {
+        printf("\nPor favor, digite o telefone (8 dígitos): ");
+        scanf("%s", telefone);
+
+        if (strlen(telefone) == 8) {
+            printf("Telefone válido.\n");
+            break;
+        } else {
+            printf("Telefone inválido. Por favor, digite novamente.\n");
+        }
+    } while (1);
+
+    
+    do {
+        printf("\nPor favor, crie uma senha (mínimo 8 caracteres): ");
+        scanf("%s", senha);
+        if (strlen(senha) >= 8) {  
+            printf("Senha validada!!\n");
+            break;
+        } else {
+            printf("Senha inválida. Digite novamente.\n");
+        }
+    } while (1);
 }
 
 int main() {
