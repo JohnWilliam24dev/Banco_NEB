f5966575b137d4edb49c0ea8aab7bb4ad48b933f
Daniel <166452209+star-Daniels@users.noreply.github.com>
2024-05-31 20:10:33 -0300

    conta pj
    Função cadastro adicionado

--- conta pj.cpp
+++ conta pj.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "crud.c"
 
 void transferir(){
 	char tipo[4], cnpj[16];
@@ -64,6 +65,93 @@ void conta(char *cnpj){
 	}
 }
 
+void cadastro() {
+    char cnpj[15];
+    char nomeempresa[19];
+    float idadeempresa;
+    char senha[20]; 
+    char pin[5];
+    char telefone[10]; 
+    char cep[9];
+    float saldo = 0;
+    float credito = 0;
+ 
+    printf("\nPor favor, digite o nome da empresa: ");
+    scanf("%s", nomeempresa);
+
+    
+    do {
+        printf("\nPor favor, digite o CNPJ (14 dígitos): ");
+        scanf("%s", cnpj);
+
+        if (strlen(cnpj) == 14) {
+            printf("CNPJ válido.\n");
+            break;
+        } else {
+            printf("CNPJ inválido. Por favor, digite novamente.\n");
+        }
+    } while (1);
+
+    printf("\nPor favor, digite a idade da empresa: ");
+    scanf("%f", &idadeempresa);
+
+
+    do {
+        printf("\nPor favor, digite o telefone (8 dígitos): ");
+        scanf("%s", telefone);
+
+        if (strlen(telefone) == 8) {
+            printf("Telefone válido.\n");
+            break;
+        } else {
+            printf("Telefone inválido. Por favor, digite novamente.\n");
+        }
+    } while (1);
+
+    
+    do {
+        printf("\nPor favor, crie uma senha (mínimo 8 caracteres): ");
+        scanf("%s", senha);
+        if (strlen(senha) >= 8) {  
+            printf("Senha validada!!\n");
+            break;
+        } else {
+            printf("Senha inválida. Digite novamente.\n");
+        }
+    } while (1);
+    do {
+        printf("\nPor favor, coloquei o pin : ");
+        scanf("%s", pin );
+        if (strlen(pin) == 4) {  
+            printf("pin validado!!\n");
+            break;
+        } else {
+            printf("Pin inválido. Digite novamente.\n");
+        }
+    } while (1);
+    do {
+        printf("\nPor favor, coloque o cep: ");
+        scanf("%s", cep);
+        if (strlen(cep) == 8) {  
+            printf("Cep validada!!\n");
+            break;
+        } else {
+            printf("Cep inválida. Digite novamente.\n");
+        }
+    } while (1);
+
+    system("cls");
+	
+	printf("Cadastro finalizado");
+	
+	   
+    system("cls");
+    printf("\nO seu saldo é: %.2f\n", saldo);
+    printf("O seu crédito é: %.2f\n", credito);  
+	
+	insert_client_legal_entire(nomeempresa, senha, pin, idadeempresa, cnpj, telefone, cep, saldo, credito); 
+}
+
 void entrar(){
    char cnpj[15], senha[15], *CNPJ;
    int da = 1;
@@ -91,14 +179,14 @@ void inicio(){
 	
 	switch (opt){
 		case 1:
-			printf("\\chamar funcao criar conta");
+			cadastro();
 			break;
 		case 2:
 			entrar();
 			break;
 		case 3:
-			printf("\\sair");
-			break;
+			printf("saindo");
+			abort();
 		default:
 			printf("Opção invalida");
 			break;
