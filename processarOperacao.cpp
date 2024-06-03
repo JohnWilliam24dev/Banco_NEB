68fe80ad5eff7af9f7fec40a809a5ca85e9252a5
Sayd Aires <159132545+saydaires@users.noreply.github.com>
2024-06-03 16:33:18 -0300

    Reformulação da função "TRANSFERÊNCIA ENTRE CONTAS"
    Reformulação da função "TRANSFERÊNCIA ENTRE CONTAS", visando substituir a excluída "DEPÓSITO ENTRE CONTAS".

---
+++ processarOperacao.cpp
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include "menuTransferencia.cpp"
+void processarOperacao(int digito) {
+	switch(digito)
+	{
+		case 0:
+			//pausarExecucao();
+			break;
+		case 1:
+			//transferencia();
+			break;
+		case 2:
+			//extratoTrasnferencia();
+			break;
+		default:
+			//printf("OPCAO INVALIDA!\n");
+			//pausarExecucao();
+	}
+}
+
