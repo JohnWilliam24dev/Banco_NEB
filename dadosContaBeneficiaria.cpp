1b669901cab2926563399f7ba1a9598464d64dd9
Sayd Aires <159132545+saydaires@users.noreply.github.com>
2024-06-05 03:43:36 -0300

    Delete dadosContaBeneficiaria.cpp

--- dadosContaBeneficiaria.cpp
+++
@@ -1,6 +0,0 @@
-#include <stdio.h>
-#include "bancoDeDados.c"
-void dadosContaBeficiaria(int numeroVetorial) {
-	printf("NOME: %s\n", pessoaFisica[numeroVetorial].nome);
-	printf("TELEFONE: %s\n", pessoaFisica[numeroVetorial].telefone);
-}
