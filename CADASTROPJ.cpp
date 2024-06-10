68b556aeb9b53e2642928a4a6e68eb908409e45b
Sayd Aires <159132545+saydaires@users.noreply.github.com>
2024-06-09 23:07:41 -0300

    (COMMIT 1) CADASTROPJ.cpp

--- CADASTROPJ.cpp
+++ CADASTROPJ.cpp
@@ -3,92 +3,110 @@
 #include <string.h>
 #include <stdlib.h>
 #include "crud.c"
-
+void pausarExecucao() {
+	printf("pressione <ENTER> para continuar");
+	getchar();
+	fflush(stdin);
+	system("cls");
+}
 void cadastro1() {
-    char cnpj[15];
-    char nomeempresa[19];
-    float idadeempresa;
+    char CNPJ[15];
+    char nomeEmpresa[19];
+    char idadeEmpresa[5];
     char senha[20]; 
     char pin[5];
     char telefone[10]; 
-    char cep[9];
+    char CEP[9];
     float saldo = 0;
     float credito = 0;
  
     printf("\nPor favor, digite o nome da empresa: ");
-    scanf("%s", nomeempresa);
+    gets(nomeEmpresa);
 
     
     do {
-        printf("\nPor favor, digite o CNPJ (14 dígitos): ");
-        scanf("%s", cnpj);
+        printf("\nPor favor, digite o CNPJ (14 dÃ­gitos): ");
+        scanf("%s", CNPJ);
+        gets(CNPJ);
 
-        if (strlen(cnpj) == 14) {
-            printf("CNPJ válido.\n");
+        if (strlen(CNPJ) == 14) {
+            printf("CNPJ vÃ¡lido.\n");
+            pausarExecucao()
             break;
         } else {
-            printf("CNPJ inválido. Por favor, digite novamente.\n");
+            printf("CNPJ invÃ¡lido. Por favor, digite novamente.\n");
+            pausarExecucao()
         }
     } while (1);
 
     printf("\nPor favor, digite a idade da empresa: ");
-    scanf("%f", &idadeempresa);
+    gets(idadeEmpresa);
 
 
     do {
-        printf("\nPor favor, digite o telefone (8 dígitos): ");
-        scanf("%s", telefone);
+        printf("\nPor favor, digite o telefone (8 dÃ­gitos): ");
+        gets(telefone);
 
         if (strlen(telefone) == 8) {
-            printf("Telefone válido.\n");
+            printf("Telefone vÃ¡lido.\n");
+            pausarExecucao()
             break;
         } else {
-            printf("Telefone inválido. Por favor, digite novamente.\n");
+            printf("Telefone invÃ¡lido. Por favor, digite novamente.\n");
+            pausarExecucao()
         }
     } while (1);
 
     
     do {
-        printf("\nPor favor, crie uma senha (mínimo 8 caracteres): ");
-        scanf("%s", senha);
+        printf("\nPor favor, crie uma senha (mÃ­nimo 8 caracteres): ");
+        gets(senha);
         if (strlen(senha) >= 8) {  
             printf("Senha validada!!\n");
+            pausarExecucao()
             break;
         } else {
-            printf("Senha inválida. Digite novamente.\n");
+            printf("Senha invÃ¡lida. Digite novamente.\n");
+            pausarExecucao()
         }
     } while (1);
     do {
         printf("\nPor favor, coloquei o pin : ");
-        scanf("%s", pin );
+        gets(pin);
         if (strlen(pin) == 4) {  
             printf("pin validado!!\n");
+            pausarExecucao()
             break;
         } else {
-            printf("Pin inválido. Digite novamente.\n");
+            printf("Pin invÃ¡lido. Digite novamente.\n");
+            pausarExecucao()
         }
     } while (1);
     do {
         printf("\nPor favor, coloque o cep: ");
-        scanf("%s", cep);
-        if (strlen(cep) == 8) {  
+        gets(CEP);
+        if (strlen(CEP) == 8) {  
             printf("Cep validada!!\n");
+            pausarExecucao()
             break;
         } else {
-            printf("Cep inválida. Digite novamente.\n");
+            printf("Cep invÃ¡lida. Digite novamente.\n");
+            pausarExecucao()
         }
     } while (1);
 
     system("cls");
 	
 	printf("Cadastro finalizado");
+	pausarExecucao()
 	
 	   
     system("cls");
-    printf("\nO seu saldo é: %.2f\n", saldo);
-    printf("O seu crédito é: %.2f\n", credito);  
+    printf("\nO seu saldo Ã©: %.2f\n", saldo);
+    printf("O seu crÃ©dito Ã©: %.2f\n", credito);  
+    pausarExecucao()
 	
-	insert_client_legal_entire(nomeempresa, senha, pin, idadeempresa, cnpj, telefone, cep, saldo, credito); 
+	insert_client_legal_entire(nomeEmpresa, senha, pin, idadeEmpresa, CNPJ, telefone, CEP, saldo, credito); 
 }
 
 int main() {
