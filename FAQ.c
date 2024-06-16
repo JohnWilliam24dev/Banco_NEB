d8f881361dfc9e3f3687beb93151288a59586e06
ELPATON <tetheu.10.alcantarasilva@gmail.com>
2024-06-16 13:17:40 -0300

    FAQ

--- FAQ.c
+++ FAQ.c
@@ -1,42 +1,49 @@
 #include <stdio.h>
 #include <stdlib.h>
-void FAQ() {
-    int choice;
+void FAQPF() {
+      int choice;
     do {
-        printf("\nBem vindo a central de ajuda!!\n");
+        printf("\nCentral de Ajuda - Pessoa Fisica\n");
         printf("Escolha uma das opcoes abaixo:\n");
         printf("1 - Problemas com cadastro\n");
         printf("2 - Problemas com login\n");
-        printf("3 - Problemas com transaçoes\n");
-        printf("4 - Outras duvidas\n");
-        printf("0 - Sair\n");
+        printf("3 - Problemas com transacoes\n");
+        printf("4 - Atualizar informações pessoais\n");
+        printf("5 - Recuperar senha\n");
+        printf("6 - Contatar suporte\n");
+        printf("0 - Voltar ao menu principal\n");
         printf("Digite sua escolha: ");
         scanf("%d", &choice);
 
         switch (choice) {
             case 1:
-                printf("\nPara problemas com cadastro, por favor, verifique se todos os campos obrigatórios estao preenchidos corretamente.\n");
+                printf("\nPara problemas com cadastro, verifique se todos os campos obrigatorios estao preenchidos corretamente.\n");
                 break;
             case 2:
-                printf("\nPara problemas com login, verifique se você está digitando seu usuário e senha corretamente. Caso tenha esquecido sua senha, fale com suporte .\n");
+                printf("\nPara problemas com login, verifique se você esta digitando seu usuario e senha corretamente. Caso tenha esquecido sua senha, utilize a opcao de recuperar senha.\n");
                 break;
             case 3:
-                printf("\nPara problemas com transaçoes, certifique-se de que possui saldo suficiente e que está utilizando os dados corretos do destinatario.\n");
+                printf("\nPara problemas com transações, certifique-se de que possui saldo suficiente e que está utilizando os dados corretos do destinatário.\n");
                 break;
             case 4:
-                printf("\nPara outras duvidas, entre em contato com nosso suporte através do email suporte@banconeb.com ou pelo telefone 0800-123-456.\n");
+                printf("\nPara atualizar suas informcoes pessoais, acesse sua conta e vá para a secao 'Perfil' para fazer as alteracoes necessarias.\n");
+                break;
+            case 5:
+                printf("\nPara recuperar sua senha, clique na opção 'Esqueci minha senha' na página de login e siga as instrucoes enviadas para o seu email.\n");
+                break;
+            case 6:
+                printf("\nPara contatar o suporte, envie um email para suporte@banco.com ou ligue para 0800-123-456.\n");
                 break;
             case 0:
-                printf("\nObrigado por utilizar a central de ajuda! Ate logo!\n");
+                printf("\nVoltando ao menu principal...\n");
                 break;
             default:
-                printf("\nOpçao invalida! Por favor, tente novamente.\n");
+                printf("\nOpcao invalida! Por favor, tente novamente.\n");
         }
     } while (choice != 0);
 }
-
 int main() {
     system("cls");  
-    FAQ();         
+    FAQPF();         
     return 0;
 }
